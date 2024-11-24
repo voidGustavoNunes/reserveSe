a8c9f251e32ff67aa4b1eb0e82dddba56c383b7d
gustavo.nunes <105016559+voidGustavoNunes@users.noreply.github.com>
2024-11-24 18:48:28 -0300

    implementando primeiras funcionalidades de restaurante

---
+++ lib/pages/tela_principal/menu_lateral/menu_lateral_cliente.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class MenuLateral extends StatefulWidget {
+  const MenuLateral({Key? key}) : super(key: key);
+
+  @override
+  _MenuLateralState createState() => _MenuLateralState();
+}
+
+class _MenuLateralState {
+
+
+}
